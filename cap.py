829a33bab553c653b1e5d285eb7fc4cf8f2fa8cc
adultmalehuman <adultmalehuman@gmail.com>
2019-12-29 13:40:07 -0400

    Simple intro to unittest

---
+++ cap.py
@@ -0,0 +1,9 @@
+def cap_text(text):
+    """
+    Input a string
+    :param text: str
+    :return: The capitalized string
+    """
+    # Unit test will reveal that 'capitalize()' does not give us the desired outcome - switch to 'title()' instead!
+    return text.capitalize()
+    # return text.title()
