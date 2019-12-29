fe4d068de4d53adec3bebea631bebadd47e57a06
adultmalehuman <adultmalehuman@gmail.com>
2019-12-29 13:23:55 -0400

    pylint utility basics

---
+++ pylint-test-before.py
@@ -0,0 +1,9 @@
+# Remember to 'pip install pylint' at command line
+# At command line: 'pylint pylint-test-before.py'
+a = 1
+b = 2
+def my_func(x,y):
+    return x+y
+print(a)
+print(B)
+my_func(a,B)
---
+++ pylint_test_after.py
@@ -0,0 +1,17 @@
+# Remember to 'pip install pylint' at command line
+# At command line: 'pylint pylint_test_after.py'
+
+"""
+This script was created while learning to use the 'pylint' utility
+"""
+
+A = 1
+B = 2
+def my_func(operand_x, operand_y):
+    """
+    Adds two numbers and returns the result
+    """
+    return operand_x+operand_y
+print(A)
+print(B)
+print(my_func(A, B))
