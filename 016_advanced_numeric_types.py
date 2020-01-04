2901b7d6ad877df275bc31d7a72c556a2c165aaa
adultmalehuman <adultmalehuman@gmail.com>
2020-01-04 16:59:52 -0400

    More with numeric types

---
+++ 016_advanced_numeric_types.py
@@ -0,0 +1,16 @@
+
+# hexadecimal support
+print(hex(246))
+print(hex(512))
+
+# binary support
+print(bin(1234))
+print(bin(128))
+print(bin(-1))
+
+# absolute values
+print(abs(-321))
+
+# rounding
+print(round(1.23456789))
+print(round(1.23456789, 4))
